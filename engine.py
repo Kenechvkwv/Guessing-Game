3b70acf8571d877d252b5e6518cd251cbed7240d
Kenechukwu Ubesie <49237742+Kenechvkwv@users.noreply.github.com>
2021-03-04 12:49:59 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,9 +1,10 @@
 import random
 def number():
-    number = random.randint(0,10)
+    number = random.randint(0,11)
     guess = int(input("guess the number:"))
     print(number)
     if guess == number:
         print("wow! Correct")
     else:
         print("Wrong, try again")
+
