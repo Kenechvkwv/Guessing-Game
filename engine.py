1cf95654351a10db43c69d7050df8678ab8c463b
Kenechukwu Ubesie <49237742+Kenechvkwv@users.noreply.github.com>
2021-03-04 12:47:52 +0100

    Add files via upload

---
+++ engine.py
@@ -0,0 +1,9 @@
+import random
+def number():
+    number = random.randint(0,10)
+    guess = int(input("guess the number:"))
+    print(number)
+    if guess == number:
+        print("wow! Correct")
+    else:
+        print("Wrong, try again")
